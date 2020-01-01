8bf813ab34fcc79d8f002db51a87c1f3699abf24
Peter Pramberger <peterpramb@member.fsf.org>
2020-01-01 16:55:12 +0100

    Fixed version check
    Ensure a supported Python version, at least at setup time.
    
    Added Python 2.6 and 3.4 to the test suite.

--- setup.py
+++ setup.py
@@ -1,14 +1,42 @@
 #!/usr/bin/env python
 
-from setuptools import setup
-from os import path
+import sys
+
+# Python version check
+if sys.version_info < (2, 6) or \
+        (sys.version_info >= (3,) and sys.version_info < (3, 4)):
+    error = """
+Pynagioscheck 0.1.7+ requires Python 2 >= 2.6, or Python 3 >= 3.4.
+Older Python versions were supported up to pynagioscheck 0.1.6.
+
+Python %s detected.
+
+Please upgrade your Python version, or resort to pynagioscheck 0.1.6.
+
+""" % '.'.join([str(v) for v in sys.version_info[:3]])
+
+    sys.stderr.write(error)
+    sys.exit(1)
+
+# Supported version, continue
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
 from io import open
+from os import path
 from nagioscheck import __version__ as version
 
+# Import README (the old way, for backward compatibility)
 basedir = path.abspath(path.dirname(__file__))
-with open(path.join(basedir, 'README.rst'), encoding='utf-8') as f:
+f = open(path.join(basedir, 'README.rst'), 'r', encoding='utf-8')
+try:
     long_description = f.read()
+finally:
+    f.close()
 
+# Entry point
 setup(
     name='nagioscheck',
     version=version,
@@ -29,6 +57,7 @@ setup(
         'Operating System :: POSIX',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
@@ -39,9 +68,9 @@ setup(
     keywords='Nagios Icinga plugin check monitoring',
     py_modules=['nagioscheck'],
     platforms=['Linux', 'Unix'],
-    python_requires='>=2.6, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, <4',
+    python_requires='>=2.6, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, <4',
     setup_requires=[
-        'setuptools>=28.8.0',
+        'setuptools>=24.2.0',
     ],
     extras_require={
         'test': ['tox>=3.8.0'],
